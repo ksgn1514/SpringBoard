c9cd641446709889bec7cf2c9d084c985ad41fa0
kim.sg <ksgn1514@gmail.com>
2023-11-10 14:01:19 +0900

    Hello World

---
+++ src/main/java/com/study/springboard/HelloController.java
@@ -0,0 +1,16 @@
+package com.study.springboard;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    // 초기구동확인
+	// http://localhost:8080/hello
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
--- src/main/java/com/study/springboard/SpringboardApplication.java
+++ src/main/java/com/study/springboard/SpringboardApplication.java
@@ -10,4 +10,5 @@ public class SpringboardApplication {
 		SpringApplication.run(SpringboardApplication.class, args);
 	}
 
+	
 }
